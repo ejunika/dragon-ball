f0a6f61ca93305577cf4ca2309849c11f393670f
Azaz Akhtar <azaz.akhtar@powerschool.com>
2019-05-09 21:12:32 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlaygroundComponent } from './playground/playground.component';
+import { DragonComponent } from './dragon/dragon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaygroundComponent,
+    DragonComponent
   ],
   imports: [
     BrowserModule,
